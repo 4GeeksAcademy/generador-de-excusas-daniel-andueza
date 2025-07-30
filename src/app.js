5749686d67957b101eaba2432d83fef7a34a4543
Daniel-And <danielanduezato@hotmail.com>
2025-07-30 11:14:37 +0000

    Excusas listas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 import "bootstrap";
 import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const ExcusaParts = {
+  who: ["Mi perro", "Mi abuela", "El cartero", "Un león", "Un oso"],
+  action: ["comió", "rompió", "perdió", "quemó", "enterró"],
+  what: ["mi tarea", "mi codigo", "el proyecto", "mi code space"],
+  when: ["antes de clase", "mientras dormía", "ayer por la noche", "en la madrugada"]
+}
+
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function generarExcusa() {
+  const sujeto = getRandomElement(ExcusaParts.who);
+  const verbo = getRandomElement(ExcusaParts.action);
+  const objeto = getRandomElement(ExcusaParts.what);
+  const momento = getRandomElement(ExcusaParts.when);
+
+  const excusa = `${sujeto} ${verbo} ${objeto} ${momento}`;
+  document.getElementById("excuse").innerText = excusa;
+}
+
+window.onload = function () {
+  console.log("Generador de excusas cargado");
+  generarExcusa();
 };
+window.generarExcusa = generarExcusa;
